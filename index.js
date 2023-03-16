0fd47ee76873a6e92ddaa5779d270721c54878b6
Caren Nafula Wakuloba <canafula16@gmail.com>
2023-03-16 18:29:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  
+  if (distance <= 400) {
+    return 'This one is on me!';
+  } else if (distance > 2000 && distance <= 2500){
+     return'I will gladly take your thirty bucks.';
+  } else {
+     return 'No can do.';
+  }
+ 
 }
+console.log(scuberGreetingForFeet(199));
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+   return city == "NYC" ? "Ok, sounds good." 
+           :"No go." ;
 }
+console.log(ternaryCheckCity("Pittsburgh"));
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+function switchOnCharmFromTip(status){
+   // Write your code here!
+    console.log(status);
+ let message;
+     switch (status) {
+     case "generous":
+         message="Thank you so much.";
+         break;
+        
+     case ("not as generous"):
+        //console.log(status); 
+        message='Thank you.'
+         break;
+
+      default:
+         message = 'Bye.'
+         console.log('Bye.');
+    }
+    return message
+ }
+//console.log(switchOnCharmFromTip ("thanks for everything"));
+
+ 
